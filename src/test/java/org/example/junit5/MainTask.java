8a2bfe06605cad0aa0354fcd544e9df9c22664e9
Archana Balakrishna Lokhande <145834338+Archana-547@users.noreply.github.com>
2024-04-01 00:59:23 +0300

    Task completed

--- src/test/java/org/example/junit5/MainTask.java
+++ src/test/java/org/example/junit5/MainTask.java
@@ -25,7 +25,7 @@ public class MainTask {
         Selenide.open("https://www.playtech.ee"); // 1. Open a web browser at the URL https://www.playtech.ee;
         $x("//html/body/header/div/nav/a[1]").click(); // 2. Click on the “Who we are” tab.
         $x("/html/body/main/section[4]").should(Condition.exist, Condition.visible); // 3. Verified if the “Global presence” is shown on the page.
-        Selenide.closeWebDriver();
+        Selenide.closeWebDriver(); //4. closing the browser
 
 
     }
@@ -40,7 +40,7 @@ public class MainTask {
     }
 
     @Test
-    public void languageSelector () { //Switch the web page language from EN to ET. Repeat the main task.
+    public void languageSelector () { // 2. Switch the web page language from EN to ET. Repeat the main task.
         // Open the website
         Selenide.open("https://www.playtech.ee");
         $x("/html/body/header/div/div/div/div[1]").click();
@@ -48,7 +48,7 @@ public class MainTask {
 
     }
  @Test
- public void ExportGlobalPresenceTextToFile(){  //  2. Export main task step 3’s result as .txt file.
+ public void ExportGlobalPresenceTextToFile(){  //  3. Export main task step 3’s result as .txt file.
      Selenide.open("https://www.playtech.ee");
      $x("//html/body/header/div/nav/a[1]").click();
      String GlobalPresenceText = $x("/html/body/main/section[4]").getText();
